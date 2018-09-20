9f8831e37cd5ab824b973b34f82613e077f227a0
Juan David Fernández Moreno <judfernandezmo@unal.edu.co>
2018-09-20 16:54:19 -0500

    casi procedimiento

--- src/Solucion primer punto.sql
+++ src/Solucion primer punto.sql
@@ -1,4 +1,7 @@
---Solucion numeral a primer punto
+--SOLUCION PRIMER PUNTO:
+
+----------------------------------------------------------------------------------------------------------------------------------------------------
+--Solucion numeral A primer punto
 
 -- Funcion que recibe el sucpadre del elemento y
 -- devuelve la suma de las ganancias de las sucursales padre.
@@ -61,4 +64,23 @@ BEGIN
 	IF ganancia_p < :NEW.ganancia THEN
 		RAISE_APPLICATION_ERROR(-20505, '¡Ganancia insertada mayor a suma de ganancia de los padres!');
 	END IF;
+END;
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------
+--Solucion numeral B primer punto
+
+--Procedimiento que recibe lista de niveles
+--y obtiene la suma de las ganancias de dichos niveles
+CREAT OR REPLACE PROCEDURE suma_niveles @codes VARCHAR(MAX)
+IS 
+	arreglo tipo_array;
+	i NUMBER;
+BEGIN
+	DBMS_OUTPUT.PUT_LINE(arreglo.COUNT);
+	i:= arreglo.FIRST;
+	WHILE i IS NOT NULL LOOP
+		DBMS_OUTPUT.PUT_LINE('Pos:  ' || i || '  Val:  ' || arreglo(i));
+		i := arreglo.NEXT(i);
+	END LOOP;
 END;

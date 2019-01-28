27987a8c591cd3f58a5deb6e8b47e6cf164ce211
scadavidg <31990704+scadavidg@users.noreply.github.com>
2019-01-28 14:34:53 -0500

    punto 2.c casi listo
    cambie el algoritmo por completo ,  tengo fallas en ciertos ejemplos del caso de ciclos infinitos

--- src/Solucion segundo punto.sql
+++ src/Solucion segundo punto.sql
@@ -7,20 +7,20 @@
 <?xml version="1.0" encoding="UTF-8" ?>
 <grafo>
   <enlace NO = "1">
-    <costviaje>  </costviaje>
-    <ptopartida>  </ptopartida>
-    <ptollegada>  </ptollegada>
+    <nodo1> </nodo1>
+    <costo> </costo>        
+    <nodo2> </nodo2>
   </enlace>
   <enlace NO = "2">
-    <costviaje>  </costviaje>
-    <ptopartida>  </ptopartida>
-    <ptollegada>  </ptollegada>
+    <nodo1> </nodo1>
+    <costo> </costo>        
+    <nodo2> </nodo2>
   </enlace>
   ...
   <enlace NO = "n">
-    <costviaje>  </costviaje>
-    <ptopartida>  </ptopartida>
-    <ptollegada>  </ptollegada>
+   <nodo1> </nodo1>
+    <costo> </costo>        
+    <nodo2> </nodo2>
   </enlace>
 </grafo>
 
@@ -104,11 +104,148 @@
   WHEN NO_DATA_FOUND THEN
     DBMS_OUTPUT.PUT_LINE('FINISH PROCEDURE');
   END;
-  /
+   /
 
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------
 --Solucion numeral C segundo punto
 
-CREATE OR REPLACE PROCEDURE posibles_rutas(codred IN red.id_red%TYPE, 
-  codciudad1 IN NUMBER, codciudad2 IN NUMBER)
+--creacion de tabla auxiliar para ordenar las rutas entre ciudades
+ CREATE TABLE rutasfinales(costo NUMBER(4),ruta VARCHAR(100) PRIMARY KEY) ;
+ --creacion de tabla auxiliar para controlar las ciudades repetidas en las rutas
+ CREATE TABLE nodos_de_ruta(nodo NUMBER (4) PRIMARY KEY NOT NULL ,indexx NUMBER (4) NOT NULL UNIQUE );
+
+
+-- procedimiento para imprimir los resultados
+ CREATE OR REPLACE PROCEDURE imprimir 
+  IS 
+    CURSOR rutas IS select * from rutasfinales order by costo DESC;
+  BEGIN              
+        FOR r1_i IN rutas LOOP
+         DBMS_OUTPUT.PUT_LINE(r1_i.ruta|| ' Total '||r1_i.costo);
+        END LOOP;     
+
+  END;
+  /
+
+--procedimiento que itera sobre la red en la busqueda de las rutas posibles entre dos ciudades
+
+ CREATE OR REPLACE PROCEDURE rutas
+    (red_id IN red.id_red%TYPE,nodoi IN NUMBER,nodof IN NUMBER,ruta IN VARCHAR,costot IN NUMBER, i IN NUMBER)
+   IS
+   CURSOR red IS  SELECT nodo1,costo,nodo2 
+                    FROM red , (XMLTABLE('/grafo/enlace'  
+                                       PASSING red.grafo_rutas
+                                       COLUMNS  
+                                          nodo1  varchar2(20)    PATH './nodo1',  
+                                          costo  varchar2(20)    PATH './costo',  
+                                          nodo2  varchar2(20)    PATH './nodo2'
+                                   ) xmlt)
+                    WHERE id_red=red_id;
+    rutar  VARCHAR(20) := INITCAP('');                
+    costor NUMBER;
+    j NUMBER;
+    val NUMBER;
+    
+   BEGIN
+
+    FOR red_i IN red LOOP
+    DBMS_OUTPUT.PUT_LINE(red_i.nodo1|| ' - '||red_i.nodo2 ||  ' vs ' ||nodoi|| ' - '||nodof); 
+    SELECT COUNT(*) INTO val FROM nodos_de_ruta WHERE nodo=red_i.nodo2 or nodo=red_i.nodo1 ;
+      IF red_i.nodo1 = nodoi  and red_i.nodo2 = nodof THEN
+        DBMS_OUTPUT.PUT_LINE('penultimo t('|| i ||  ')='||red_i.nodo1);
+        INSERT INTO nodos_de_ruta VALUES(red_i.nodo1,i);
+        j:=i+1;
+        DBMS_OUTPUT.PUT_LINE('ultimo t('|| j ||  ')='||red_i.nodo2);
+        INSERT INTO nodos_de_ruta VALUES(red_i.nodo2,j);
+
+        rutar:= ruta||nodoi ||'-' || nodof;
+        costor:=costot+red_i.costo;
+        INSERT INTO rutasfinales VALUES(costor,rutar);
+        delete from nodos_de_ruta;
+        Exit;
+
+
+      ELSIF red_i.nodo1 = nodoi THEN
+        IF val = 0 THEN
+          DBMS_OUTPUT.PUT_LINE('validacion pasa t('|| i ||  ')='||red_i.nodo1); 
+          INSERT INTO nodos_de_ruta VALUES(red_i.nodo1,i);
+          costor:=costot+red_i.costo;
+          j:=i+1;
+          rutar:=ruta || nodoi ||'-';
+          DBMS_OUTPUT.PUT_LINE('envio('|| red_i.nodo2 ||  ','||nodof||')'); 
+          DBMS_OUTPUT.PUT_LINE('_______________________________________'); 
+          rutas(red_id,red_i.nodo2,nodof,rutar,costor,j);
+        ELSE    
+          delete from nodos_de_ruta;
+          CONTINUE;
+        END IF;
+
+      END IF;
+
+
+    END LOOP;
+        
+   END;
+   /
+
+-- prodecimiento que recibe los datos iniciales  y llama los demas procedimientos para su implementacion
+
+CREATE OR REPLACE PROCEDURE pre_busqueda
+  (red_id IN red.id_red%TYPE,nodoi IN NUMBER,nodof IN NUMBER)
+  IS 
+    TYPE historial IS TABLE OF PLS_INTEGER INDEX BY PLS_INTEGER; 
+    CURSOR red_v1 IS  SELECT nodo1,costo,nodo2 
+                  FROM red , (XMLTABLE('/grafo/enlace'  
+                                     PASSING red.grafo_rutas
+                                     COLUMNS  
+                                        nodo1  varchar2(20)    PATH './nodo1',  
+                                        costo  varchar2(20)    PATH './costo',  
+                                        nodo2  varchar2(20)    PATH './nodo2'
+                                 ) xmlt)
+                  WHERE id_red=red_id and nodo1=nodoi;
+
+    CURSOR red_v2 IS  SELECT nodo1,costo,nodo2 
+                  FROM red , (XMLTABLE('/grafo/enlace'  
+                                     PASSING red.grafo_rutas
+                                     COLUMNS  
+                                        nodo1  varchar2(20)    PATH './nodo1',  
+                                        costo  varchar2(20)    PATH './costo',  
+                                        nodo2  varchar2(20)    PATH './nodo2'
+                                 ) xmlt)
+                  WHERE id_red=red_id and nodo2=nodof;
+
+    red_v1i red_v1%ROWTYPE;
+    red_v2i red_v2%ROWTYPE;
+  BEGIN
+     IF(nodoi=nodof) THEN
+       DBMS_OUTPUT.PUT_LINE('no hay rutas');
+     ELSE
+      delete from rutasfinales;
+      delete from nodos_de_ruta;  
+
+        OPEN red_v1;
+        FETCH red_v1 INTO red_v1i;
+        
+        IF(red_v1%FOUND)THEN
+
+          OPEN red_v2;
+            FETCH red_v2 INTO red_v2i;
+        
+            IF(red_v2%FOUND)THEN  
+
+              rutas(red_id,nodoi,nodof,'',0,1);
+              imprimir();  
+             
+            ELSE
+              DBMS_OUTPUT.PUT_LINE('Nodo fuera de la red');
+            END IF;
+          CLOSE red_v2;
+          
+        ELSE
+          DBMS_OUTPUT.PUT_LINE('Nodo fuera de la red');
+        END IF;
+       CLOSE red_v1;
+     END IF;  
+  END;
+  /

a65d73ec23a1bd49b6ad29745136b58a9034e6f9
Juan David Fernández Moreno <judfernandezmo@unal.edu.co>
2019-01-30 20:40:43 -0500

    mas ejemplos
    eso mismo mas ejemplos :v:

--- src/Segundo punto.sql
+++ src/Segundo punto.sql
@@ -44,3 +44,76 @@ INSERT INTO red VALUES(5,'ejemplo', XMLTYPE('
         <nodo2>12</nodo2>
     </enlace>
 </grafo>'));
+
+INSERT INTO red VALUES(330,'profesor', XMLTYPE('
+<grafo>
+    <enlace NO = "1">
+        <nodo1>1</nodo1>
+        <costo>100</costo>        
+        <nodo2>2</nodo2>
+    </enlace>
+    <enlace NO = "2">
+        <nodo1>1</nodo1>
+        <costo>50</costo>        
+        <nodo2>3</nodo2>
+    </enlace>
+     <enlace NO = "3">
+        <nodo1>1</nodo1>
+        <costo>90</costo>        
+        <nodo2>4</nodo2>
+    </enlace>
+    <enlace NO = "4">
+        <nodo1>2</nodo1>
+        <costo>20</costo>        
+        <nodo2>4</nodo2>
+    </enlace>
+    <enlace NO = "5">
+        <nodo1>3</nodo1>
+        <costo>30</costo>        
+        <nodo2>4</nodo2>
+    </enlace>
+     <enlace NO = "6">
+        <nodo1>4</nodo1>
+        <costo>90</costo>        
+        <nodo2>5</nodo2>
+    </enlace>
+    <enlace NO = "7">
+        <nodo1>5</nodo1>
+        <costo>10000</costo>        
+        <nodo2>1</nodo2>
+    </enlace>
+</grafo>'));
+
+INSERT INTO red VALUES(33,'ejemplo2', XMLTYPE('
+<grafo>
+    <enlace NO = "1">
+        <nodo1>1</nodo1>
+        <costo>100</costo>        
+        <nodo2>2</nodo2>
+    </enlace>
+    <enlace NO = "2">
+        <nodo1>1</nodo1>
+        <costo>50</costo>        
+        <nodo2>3</nodo2>
+    </enlace>
+     <enlace NO = "3">
+        <nodo1>1</nodo1>
+        <costo>90</costo>        
+        <nodo2>4</nodo2>
+    </enlace>
+    <enlace NO = "4">
+        <nodo1>4</nodo1>
+        <costo>20</costo>        
+        <nodo2>2</nodo2>
+    </enlace>
+    <enlace NO = "5">
+        <nodo1>3</nodo1>
+        <costo>30</costo>        
+        <nodo2>4</nodo2>
+    </enlace>
+     <enlace NO = "6">
+        <nodo1>4</nodo1>
+        <costo>90</costo>        
+        <nodo2>5</nodo2>
+    </enlace>
+</grafo>'));
